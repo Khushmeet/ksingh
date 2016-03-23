9dd3872d6bce427c8e96d0a21d6813a61ec8d496
khushmeet <khushmeetsingh1996@gmail.com>
2016-03-23 11:15:50 +0530

    tags api  defined

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -17,6 +17,7 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
+  require('../api/tag/tag.socket').register(socket);
   require('../api/article/article.socket').register(socket);
 
 
--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,7 @@ import path from 'path';
 
 export default function(app) {
   // Insert routes below
+  app.use('/api/tags', require('./api/tag'));
   app.use('/api/articles', require('./api/article'));
   app.use('/api/users', require('./api/user'));
 
