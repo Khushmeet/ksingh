61b1932c661a03e42347e34076301795f7ebba21
khushmeet <khushmeetsingh1996@gmail.com>
2016-03-25 17:27:48 +0530

    search page ui improvements

--- client/app/search/search.controller.js
+++ client/app/search/search.controller.js
@@ -1,12 +1,21 @@
 'use strict';
 
 angular.module('ksinghApp')
-  .controller('SearchCtrl', function ($scope, searchService) {
-    searchService.get({tag:'website'},
-      function success(response){
-        console.log(response);
-      },
-      function error(error) {
-        console.log(error);
-      });
+  .controller('SearchCtrl', function($scope, searchService) {
+    $scope.show = undefined;
+    $scope.search = function(form,text) {
+      if (form.$valid) {
+        $scope.show = text;
+        searchService.get({
+            tag: text
+          },
+          function success(response) {
+            console.log(response);
+            $scope.list = response;
+          },
+          function error(error) {
+            console.log(error);
+          });
+      }
+    };
   });
