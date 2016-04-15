8a68da90c275f93dc72790b3b7fc1666fa7cba9f
Khushmeet <khushmeetsingh1996@gmail.com>
2016-04-16 02:03:59 +0530

    navbar directive changed

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -2,19 +2,7 @@
 
 class NavbarController {
   //start-non-standard
-  menu = [{
-    'title': 'Home',
-    'state': 'main'
-  }];
-
-  isCollapsed = true;
-  //end-non-standard
-
-  constructor(Auth) {
-    this.isLoggedIn = Auth.isLoggedIn;
-    this.isAdmin = Auth.isAdmin;
-    this.getCurrentUser = Auth.getCurrentUser;
-  }
+  
 }
 
 angular.module('ksinghApp')
